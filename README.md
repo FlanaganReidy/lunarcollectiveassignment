7055976ed6ad2dcf5b2460613003b4f13cec386f
Shannon <reidy.pf@gmail.com>
2017-12-07 17:20:08 -0600

    created db locally and linked it via mongoclient

---
+++ README.md
@@ -0,0 +1,6 @@
+This is an SPA for my Lunar Collective interview.
+
+Technologies used:
+  Mustache for views
+  Express/nodejs for back end
+  Mongoose for ORM with MongoDB
--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://localhost:27017/textblockdb');
+const MongoClient = require('mongodb').MongoClient,
+  assert = require('assert');
+const ObjectId = require('mongodb').ObjectID;
+
 
 const app = express();
 
 app.engine('mustache', mustacheExpress());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
 app.set('views', './views');
 app.set('view engine', 'mustache');
 
+app.get('/', function(req,res){
+res.render('index');
+})
+app.post('/', function(req,res){
+
+})
 app.listen(3000, function(){
-  Console.log('Successfully started Express Application')
+  console.log('Successfully started Express Application');
 })
